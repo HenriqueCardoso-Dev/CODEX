b283935a8ed4e89a4c61f07012decee326695f90
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2021-03-24 15:48:36 -0300

    update-class-content-page

--- codex-app/src/app/pages/class-content/class-content.page.ts
+++ codex-app/src/app/pages/class-content/class-content.page.ts
@@ -1,3 +1,4 @@
+import { classContentBox, describeClassContent,} from 'src/interfacesModels/classContentPage';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassContentPage implements OnInit {
 
-  constructor() { }
+  private classesContent : classContentBox[];
+  private describeClasses : describeClassContent[];
+
+  constructor() { 
+    this.describeClasses = [
+      {describe: 'Seja Bem-Vindo a primeira aula de HTML. Na aula de hoje estaremos vendo uma introdução da linguagem e sua sintaxe.'}
+    ]
+    this.classesContent = [
+      {id: 1, name:'Exercício 01', describe: 'Introdução a linguagem', image: 'assets/images/languages/html/html-t.jpg'},
+      {id: 2, name:'Exercício 02', describe: 'Introdução a sintaxe', image: 'assets/images/languages/html/html-t.jpg'},
+      {id: 3, name:'Exercício 03', describe: 'Revisão da aula', image: 'assets/images/languages/html/html-p.jpg'}
+    ]
+  }
 
   ngOnInit() {
   }
---
+++ codex-app/src/interfacesModels/classContentPage.ts
@@ -0,0 +1,9 @@
+export interface describeClassContent{
+  describe: string;
+}
+export interface classContentBox{
+  id: number;
+  name: string;
+  describe: string;
+  image: string;
+}
