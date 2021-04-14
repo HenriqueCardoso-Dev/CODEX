ae04c9ce5934ed0cf0e37e79c05d469e4bec585d
Marcelo Jimenes <marcelojimenes1506@gmail.com>
2021-04-14 20:36:22 -0300

    register class select

--- codex-app/src/app/pages/register-classes/register-classes.page.ts
+++ codex-app/src/app/pages/register-classes/register-classes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LanguagesService } from 'src/app/services/api/languages.service';
 
 @Component({
   selector: 'app-register-classes',
@@ -12,14 +13,19 @@ export class RegisterClassesPage implements OnInit {
   private languages;
 
   constructor(
-    private router : Router
+    private router : Router,
+    private langService: LanguagesService
 
   ){
 
   }
 
   ngOnInit() {
-    this
+    this.langService.getAll().subscribe(response => {
+      if (response[0].nome) {
+        this.languages = response;
+      }
+    })
   }
 
   goToDash() {
